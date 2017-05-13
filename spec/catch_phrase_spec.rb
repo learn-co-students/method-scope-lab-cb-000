34e2bb628d41d1c017bd7f9c4f2480684f4a15c1
Mohsen Danesh Pajouh <mohsendp@gmail.com>
2017-05-13 11:15:18 +0000

    Done.

--- spec/catch_phrase_spec.rb
+++ spec/catch_phrase_spec.rb
@@ -1,8 +1,15 @@
 require "spec_helper"
 
-describe "#catch_phrase" do 
-  it "puts out a catch phrase" do 
-    phrase = "It's-a me, Mario!"
-    expect{catch_phrase}.to output("It's-a me, Mario!\n").to_stdout
+describe "#catch_phrase" do
+  defualt_phrase = "It's-a me, Mario!\n" #puts return the string with \n at the end!
+  it "puts out the defualt catch phrase" do
+    expect{catch_phrase}.to output(defualt_phrase).to_stdout
   end
+  #A block that is defined inside another block has access
+  # to the local variable of that parent block
+  it "puts out the given phrase" do
+    phrase = "salam\n"
+    expect{catch_phrase phrase}.to output(phrase).to_stdout
+  end
+
 end
